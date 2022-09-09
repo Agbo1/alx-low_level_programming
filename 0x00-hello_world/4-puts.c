8d8b46d4580182fc9ccd4c3738df6f00d6f95393
Agbo1 <agboidoko20@gmail.com>
2022-09-08 17:52:52 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <studio.h>
-
+#include<stdio.h>
 /**
-* main - print the string in the put function 
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+* main - Entry point
+* Description: prints "\"Programming is like building a multilingual puzzle"*
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
