72be0ca8284eb3ceefc448d3aaf7b30ddf6323ec
Agbo1 <agboidoko20@gmail.com>
2022-09-10 06:50:18 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,18 +6,18 @@
  * Return: 1
  */
 
-int main(void)  
+int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";  
-long l = 59;  
-long fd = 1;  
-long syscall = 1;  
-long ret = 0;  
-__asm__ ("syscall"  
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
 : "=a" (ret)
 : "a" (syscall),
 "D" (fd),
 "S" (s),
-"d" (l));  
+"d" (l));
 return (1);
 }
